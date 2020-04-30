d70de13fcd2e932ba5269758f28ee032ed13a8f4
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-04-30 15:52:27 -0400

    Added jumbotron to page

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron() {
+  return (
+    <div>
+      <div className="jumbotron">
+        <h1 className="display-4">Employee Directory</h1>
+        <p className="lead">This is a list of all your employees!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Jumbotron;
